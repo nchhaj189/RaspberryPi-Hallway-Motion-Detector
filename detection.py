99b83368f5c833e6f60b3ad09c0234b5a93702f7
Nipun Chhajed <nipunchhajed@gmail.com>
2019-08-22 13:54:04 -0700

    added support for 2 LEDs and fixed all errors to make it work.

--- detection.py
+++ detection.py
@@ -13,15 +13,16 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(7, GPIO.OUT)
-
+GPIO.setup(11, GPIO.OUT)
+GPIO.output([7,11],0)
 #PiCamera specific setup
-with open('camera_conf.json') as json:
-	camconf = json.load(json)
+with open('camera_conf.json') as json_file:
+    camconf = json.load(json_file)
 
 picam = PiCamera()
 picam.resolution = tuple(camconf["resolution"])
 picam.framerate = camconf["fps"]
-rawStream = PiRGBArray(picam, size=tuple(conf["resolution"]))
+rawStream = PiRGBArray(picam, size=tuple(camconf["resolution"]))
 print("Camera is getting configured")
 time.sleep(camconf["camera_warmup_time"])
 
@@ -30,7 +31,7 @@ peopleCounter = 0
 
 for fr in picam.capture_continuous(rawStream, format="bgr", use_video_port=True):
     frame = fr.array
-    currentTime = datetime.datetime.now()
+    timestamp = datetime.datetime.now()
     text = "No one in sight"
     frame = imutils.resize(frame, width=500)
     grayScaledFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -59,16 +60,16 @@ for fr in picam.capture_continuous(rawStream, format="bgr", use_video_port=True)
         text = "Motion detected in hallway"
 
     ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
-	cv2.putText(frame, "Status: {}".format(text), (10, 20),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-	cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-		0.35, (0, 0, 255), 1)    
+    cv2.putText(frame, "Status: {}".format(text), (10, 20),
+    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+        0.35, (0, 0, 255), 1)    
     
     if text == "Motion detected in hallway":
         peopleCounter += 1
-        GPIO.output(7,1)
+        GPIO.output([7,11],1)
     else:
-        GPIO.output(7,0)
+        GPIO.output([7,11],0)
     
     if camconf["show_video"]:
         cv2.imshow("Hallway Cam", frame)
@@ -77,6 +78,6 @@ for fr in picam.capture_continuous(rawStream, format="bgr", use_video_port=True)
         if key == ord("q"):
             GPIO.cleanup()
             break
-    rawCapture.truncate(0)        
-
+    rawStream.truncate(0)
+    print(peopleCounter)
 GPIO.cleanup()
